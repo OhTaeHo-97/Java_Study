88f40b6a070c71d4e25a61a2bfe5d70be9f07708
OhTaeHo-97 <dhxogh123123@naver.com>
2022-03-02 23:02:18 +0900

    baekjoon_exercise_22/03/02

--- Baekjun/src/baekjun/baekjun1010.java
+++ Baekjun/src/baekjun/baekjun1010.java
@@ -1,81 +1,59 @@
 package baekjun;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class baekjun1010 {
-	static int calculate(int n, int m, int index) {
-        int sum = 0;
-        for(int i = m - n + 1 - index - 1; i > 0; i--) {
-            sum += i;
-        }
-        int temp = sum;
-        for(int i = m - n + 1 - index - 1; i > 0; i--) {
-            temp -= i;
-            sum += temp;
-        }
-        return sum;
-    }
+	public int combination(int n, int m) {
+		int m_temp = m;
+		long m_mul = 1;
+		long n_mul = 1;
+		for(int i = 1; i <= n; i++) {
+			m_mul *= m_temp;
+			n_mul *= i;
+			m_temp--;
+		}
+		return (int)(m_mul / n_mul);
+	}
+	public int calculateNum(int n, int m) {
+		if(n == m) {
+			return 1;
+		}
+		if(n > (m / 2)) {
+			int dif = m - n;
+			return combination(dif, m);
+		} else {
+			return combination(n, m);
+		}
+	}
 	
-//	static long combination(int m, int n) {
-//		long result = 1;
-//		long divisor = 1;
-//		for(int i = m; i > m - n; i--) {
-//			result *= i;
-//		}
-//		System.out.println("result = " + result);
-//		
-//		for(int i = 1; i <= n; i++) {
-//			divisor *= i;
-//		}
-//		System.out.println("divisor = " + divisor);
-//		return result / divisor;
-//	}
+	public ArrayList<Integer> putBridge(ArrayList<ArrayList<Integer>> testcase) {
+		ArrayList<Integer> numBridge = new ArrayList<Integer>();
+		for(int i = 0; i < testcase.size(); i++) {
+			numBridge.add(calculateNum(testcase.get(i).get(0), testcase.get(i).get(1)));
+		}
+		return numBridge;
+	}
 	
-	static long fact(int m) {
-        if(m == 0)
-            return 1;
-        return m * fact(m - 1);
-    }
-    static long combination(int m, int n) {
-        return (fact(m) / (fact(n) * fact(m - n)));
-    }
-	
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int num = sc.nextInt();
-//        for(int i = 0; i < num; i++) {
-//            int n = sc.nextInt();
-//            int m = sc.nextInt();
-//            if(n < 0 || n >= 30 || m < 0 || m >= 30 || n > m) {
-//                System.err.println("잘못된 숫자를 입력하셨습니다.");
-//                System.exit(1);    
-//            }
-//            if(n == 1) {
-//                System.out.println(m);
-//            } else if(m == n) {
-//                System.out.println("1");
-//            } else {
-//                int sum = 0;
-//                for(int j = 1; j <= (m - n + 1); j++) {
-//                    sum += calculate(n, m, j);
-//                }
-//                System.out.println(sum);
-//            }
-//        }
-//    	System.out.println(combination(29, 13));
-//    }
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        long[] result = new long[num];
-        for(int i = 0; i < num; i++) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            result[i] = combination(m, n);
-        }
-        
-        for(int i = 0; i < num; i++) {
-            System.out.println(result[i]);
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		ArrayList<ArrayList<Integer>> testcase = new ArrayList<ArrayList<Integer>>();
+		for(int i = 0; i < num; i++) {
+			ArrayList<Integer> temp = new ArrayList<Integer>();
+			String site = br.readLine();
+			StringTokenizer st = new StringTokenizer(site);
+			temp.add(Integer.parseInt(st.nextToken()));
+			temp.add(Integer.parseInt(st.nextToken()));
+			testcase.add(temp);
+		}
+		baekjun1010 b = new baekjun1010();
+		ArrayList<Integer> numBridge = b.putBridge(testcase);
+		for(int i = 0; i < numBridge.size(); i++) {
+			System.out.println(numBridge.get(i));
+		}
+	}
 }
