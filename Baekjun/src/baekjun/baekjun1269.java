a619249ef9154a9fa591e59a7863c7fbd7446bbd
OhTaeHo-97 <dhxogh123123@naver.com>
2022-03-14 00:08:25 +0900

    baekjoon_exercise_22/03/13(update)

--- Baekjun/src/baekjun/baekjun1269.java
+++ Baekjun/src/baekjun/baekjun1269.java
@@ -6,29 +6,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class baekjun1269 {
 	public int getNumSet(int num_a, int num_b, String list_a, String list_b) {
 		ArrayList<Integer> component_a = new ArrayList<Integer>();
 		ArrayList<Integer> component_b = new ArrayList<Integer>();
-		ArrayList<Integer> copy_a = new ArrayList<Integer>();
-		ArrayList<Integer> copy_b = new ArrayList<Integer>();
 		StringTokenizer st = new StringTokenizer(list_a);
 		for(int i = 0; i < num_a; i++) {
 			component_a.add(Integer.parseInt(st.nextToken()));
-			copy_a.add(component_a.get(i));
 		}
 		st = new StringTokenizer(list_b);
 		for(int i = 0; i < num_b; i++) {
 			component_b.add(Integer.parseInt(st.nextToken()));
-			copy_b.add(component_b.get(i));
 		}
-		copy_a.removeAll(component_b);
-		copy_b.removeAll(component_a);
-		copy_a.addAll(copy_b);
-		HashSet<Integer> set = new HashSet<Integer>(copy_a);
+		TreeSet set = new TreeSet();
+		for(int i = 0; i < component_a.size(); i++) {
+			if(!set.add(component_a.get(i))) {
+				set.remove(component_a.get(i));
+			}
+		}
+		for(int i = 0; i < component_b.size(); i++) {
+			if(!set.add(component_b.get(i))) {
+				set.remove(component_b.get(i));
+			}
+		}
+		
 		return set.size();
 	}
 	
@@ -41,7 +45,10 @@ public class baekjun1269 {
 		int num_b = Integer.parseInt(st.nextToken());
 		String list_a = br.readLine();
 		String list_b = br.readLine();
+		br.close();
 		baekjun1269 b = new baekjun1269();
-		System.out.println(b.getNumSet(num_a, num_b, list_a, list_b));
+		bw.write(b.getNumSet(num_a, num_b, list_a, list_b) + "\n");
+		bw.flush();
+		bw.close();
 	}
 }
