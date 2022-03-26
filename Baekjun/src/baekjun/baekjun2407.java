a3e972937242e98ef429e5368033ee8b2a664578
OhTaeHo-97 <dhxogh123123@naver.com>
2022-03-26 23:46:19 +0900

    baekjoon_exercise_22/03/25(Edit)

--- Baekjun/src/baekjun/baekjun2407.java
+++ Baekjun/src/baekjun/baekjun2407.java
@@ -1,29 +1,37 @@
 package baekjun;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 
 public class baekjun2407 {
+	public BigInteger getCombination(int n, int m) {
+		BigInteger num1 = BigInteger.ONE;
+		BigInteger num2 = BigInteger.ONE;
+		for(int i = 0; i < m; i++) {
+			num1 = num1.multiply(new BigInteger(String.valueOf(n - i)));
+			num2 = num2.multiply(new BigInteger(String.valueOf(i + 1)));
+		}
+		BigInteger result = num1.divide(num2);
+		return result;
+	}
+	
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] inputs = br.readLine().split(" ");
-
-        int n = Integer.parseInt(inputs[0]);
-        int m = Integer.parseInt(inputs[1]);
-
-        BigInteger n1 = BigInteger.ONE;
-        BigInteger n2 = BigInteger.ONE;
-
-        for (int i = 0; i < m; i++) {
-            n1 = n1.multiply(new BigInteger(String.valueOf(n - i)));
-            n2 = n2.multiply(new BigInteger(String.valueOf(i + 1)));
-        }
-
-        BigInteger answer = n1.divide(n2);
-
-        System.out.println(answer);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        br.close();
+        StringTokenizer st = new StringTokenizer(input);
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        baekjun2407 b = new baekjun2407();
+        bw.write(b.getCombination(n, m) + "\n");
+        bw.flush();
+        bw.close();
     }
 }
