a0029b31fbc99d792fe92a209fcd42572fd0f91a
OhTaeHo-97 <dhxogh123123@naver.com>
2022-04-09 22:35:41 +0900

    baekjoon_exercise_22/04/09

---
+++ Baekjun/src/baekjun/baekjun2410.java
@@ -0,0 +1,54 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class baekjun2410 {
+	public long getReciprocalNum(int num) {
+//		if(num == 1) {
+//			return 1;
+//		}
+//		long reciprocalNum = 2;
+//		int dif = 0;
+//		int count = 0;
+//		for(int i = 2; i <= num; i += 2) {
+//			reciprocalNum += dif;
+//			if(count % 2 == 0) {
+//				dif += 2;
+//			}
+//			count++;
+//		}
+//		return reciprocalNum % 1000000000;
+		int binary_num = Integer.toBinaryString(num).length();
+		int[] reciprocal = new int[binary_num + 1];
+		long[] dp = new long[num + 1];
+		int N = 1000000000;
+		dp[0] = 1;
+		reciprocal[1] = 1;
+		for(int i = 2; i <= binary_num; i++) {
+			reciprocal[i] = reciprocal[i - 1] * 2;
+		}
+		for(int i = 1; i <= binary_num; i++) {
+			for(int j = reciprocal[i]; j <= num; j++) {
+				dp[j] = dp[j] % N + dp[j - reciprocal[i]] % N;
+				if(dp[j] > N)
+					dp[j] %= N;
+			}
+		}
+		return dp[num];
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int num = Integer.parseInt(br.readLine());
+		br.close();
+		baekjun2410 b = new baekjun2410();
+		bw.write(b.getReciprocalNum(num) + "\n");
+		bw.flush();
+		bw.close();
+	}
+}
