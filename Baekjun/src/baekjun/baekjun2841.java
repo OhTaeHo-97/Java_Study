e061a71562b408d59e5f38898a49bda7bb7b8528
OhTaeHo-97 <dhxogh123123@naver.com>
2022-04-24 21:49:04 +0900

    baekjoon_exercise_22/04/24

--- Baekjun/src/baekjun/baekjun2841.java
+++ Baekjun/src/baekjun/baekjun2841.java
@@ -5,42 +5,30 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class baekjun2841 {
-	public int getMinMoveNum(Note[] melody) {
-		ArrayList<Integer>[] frets = new ArrayList[6];
-		for(int i = 0; i < frets.length; i++) {
-			frets[i] = new ArrayList<Integer>();
-		}
+	public int getMinMoveNum(int[][] melody) {
+		Stack<Integer>[] frets = new Stack[6];
 		int count = 0;
+		for(int i = 0; i < frets.length; i++) {
+			frets[i] = new Stack<Integer>();
+		}
 		for(int i = 0; i < melody.length; i++) {
-			if(frets[melody[i].string_num - 1].size() == 0) {
-				frets[melody[i].string_num - 1].add(melody[i].fret);
+			if(frets[melody[i][0] - 1].size() == 0) {
+				frets[melody[i][0] - 1].push(melody[i][1]);
 				count++;
-			} else {
-				if(frets[melody[i].string_num - 1].get(frets[melody[i].string_num - 1].size() - 1) != melody[i].fret) {
-					if(frets[melody[i].string_num - 1].get(frets[melody[i].string_num - 1].size() - 1) < melody[i].fret) {
-						frets[melody[i].string_num - 1].add(melody[i].fret);
-						count++;
-					} else {
-						while(frets[melody[i].string_num - 1].size() != 0 && frets[melody[i].string_num - 1].get(frets[melody[i].string_num - 1].size() - 1) > melody[i].fret) {
-							frets[melody[i].string_num - 1].remove(frets[melody[i].string_num - 1].size() - 1);
-							count++;
-						}
-						if(frets[melody[i].string_num - 1].size() == 0) {
-							frets[melody[i].string_num - 1].add(melody[i].fret);
-							count++;
-						} else {							
-							if(frets[melody[i].string_num - 1].get(frets[melody[i].string_num - 1].size() - 1) < melody[i].fret) {
-								frets[melody[i].string_num - 1].add(melody[i].fret);
-								count++;
-							}
-						}
-					}
-				}
 			}
+			while(frets[melody[i][0] - 1].size() != 0 && frets[melody[i][0] - 1].peek() > melody[i][1]) {
+				frets[melody[i][0] - 1].pop();
+				count++;
+			}
+			if(frets[melody[i][0] - 1].size() != 0 && frets[melody[i][0] - 1].peek() == melody[i][1]) {
+				continue;
+			}
+			frets[melody[i][0] - 1].push(melody[i][1]);
+			count++;
 		}
 		return count;
 	}
@@ -52,25 +40,17 @@ public class baekjun2841 {
 		StringTokenizer st = new StringTokenizer(input);
 		int note_num = Integer.parseInt(st.nextToken());
 		int fret_num = Integer.parseInt(st.nextToken());
-		Note[] melody = new Note[note_num];
+		int[][] melody = new int[note_num][2];
 		for(int i = 0; i < melody.length; i++) {
 			input = br.readLine();
 			st = new StringTokenizer(input);
-			melody[i] = new Note(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			melody[i][0] = Integer.parseInt(st.nextToken());
+			melody[i][1] = Integer.parseInt(st.nextToken());
 		}
 		br.close();
 		baekjun2841 b = new baekjun2841();
 		bw.write(b.getMinMoveNum(melody) + "\n");
 		bw.flush();
 		bw.close();
-	}
-}
-
-class Note {
-	int string_num;
-	int fret;
-	public Note(int string_num, int fret) {
-		this.string_num = string_num;
-		this.fret = fret;
 	}
 }
