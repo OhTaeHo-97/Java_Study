fd5b0c4fba6b308a9a6f677c56cdf6aa3152bf84
OhTaeHo-97 <dhxogh123123@naver.com>
2022-05-05 22:58:59 +0900

    baekjoon_exercise_22/05/05

---
+++ Baekjun/src/baekjun/baekjun2230.java
@@ -0,0 +1,47 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class baekjun2230 {
+	public int getMinDif(int dif, int[] series) {
+		Arrays.sort(series);
+		int left = 0;
+		int right = 0;
+		int min = Integer.MAX_VALUE;
+		while(right < series.length) {
+			int difference = series[right] - series[left];
+			if(difference < dif) {
+				right++;
+				continue;
+			}
+			if(difference == dif) {
+				return dif;
+			}
+			left++;
+			min = Math.min(min, difference);
+		}
+		return min;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] input = br.readLine().split(" ");
+		int num = Integer.parseInt(input[0]);
+		int dif = Integer.parseInt(input[1]);
+		int[] series = new int[num];
+		for(int i = 0; i < num; i++) {
+			series[i] = Integer.parseInt(br.readLine());
+		}
+		br.close();
+		baekjun2230 b = new baekjun2230();
+		bw.write(b.getMinDif(dif, series) + "\n");
+		bw.flush();
+		bw.close();
+	}
+}
