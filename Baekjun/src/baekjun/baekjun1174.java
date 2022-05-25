64bacbc169b88dad93aae77ef36eff98fb178aad
OhTaeHo-97 <dhxogh123123@naver.com>
2022-05-26 00:05:47 +0900

    baekjoon_exercise_22/05/25

--- Baekjun/src/baekjun/baekjun1174.java
+++ Baekjun/src/baekjun/baekjun1174.java
@@ -5,54 +5,45 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class baekjun1174 {
-	public long getDecreasingNum(int num) {
-		if(num <= 10) {
-			return num - 1;
+	static int[] nums = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+	static ArrayList<Long> arr = new ArrayList<Long>();
+	static int num;
+	public void dfs(long sum, int index) {
+		// sum이 이전에 나온적이 없는 수라면 arr에 저장
+		if(!arr.contains(sum)) {
+			arr.add(sum);
 		}
-		int[][] dp = new int[11][10];
-		Arrays.fill(dp[1], 1);
-		int sum = 10;
-		int start = 1;
-		int len = 2;
-		int first = 0;
-		boolean isBig = false;
-		while(len <= 10) {
-			int count = 0;
-			isBig = false;
-			for(int i = start; i < 10; i++) {
-				count += dp[len - 1][start - 1];
-				dp[len][i] = count;
-				sum += count;
-				if(sum > num) {
-					first = i - 1;
-					sum -= count;
-					isBig = true;
-					break;
-				}
-			}
-			if(isBig) {
-				break;
-			} else {
-				start++;
-				len++;
-			}
+		// nums 배열 모두 탐색했으면 재귀호출 종료
+		if(index >= 10) {
+			return;
 		}
-		if(isBig) {
-			String result = Integer.toString(first);
-			
-		} else {
+		dfs(sum * 10 + nums[index], index + 1); // 현재 수 선택하는 경우
+		dfs(sum, index + 1); // 현재 수 선택하지 않는 경우
+	}
+	
+	public long getDecreasingNum() {
+		dfs(0, 0);
+		Collections.sort(arr);
+		if(num > 1023) {
 			return -1;
+		} else {
+			return arr.get(num - 1);
 		}
-		return 1;
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int num = Integer.parseInt(br.readLine());
+		num = Integer.parseInt(br.readLine());
 		br.close();
+		baekjun1174 b = new baekjun1174();
+		bw.write(b.getDecreasingNum() + "\n");
+		bw.flush();
+		bw.close();
 	}
 }
