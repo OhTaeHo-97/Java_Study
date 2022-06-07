79b73588f9442bd0a00897e07d2415afe3f21ed9
OhTaeHo-97 <dhxogh123123@naver.com>
2022-06-07 17:45:42 +0900

    SWAcademy_exercise_22/06/06

--- SWExpertAcademy/src/SWExoertAcademy/SW9778.java
+++ SWExpertAcademy/src/SWExoertAcademy/SW9778.java
@@ -24,16 +24,26 @@ public class SW9778 {
 		}
 		int high = 0;
 		int low = 0;
-		for(int i = dif; i <= 11; i++) {
-			
+		for(int i = 2; i <= 11; i++) {
+			if(dif - i < 0) {
+				high += map.get(i);
+			} else {
+				low += map.get(i);
+			}
+		}
+		if(high >= low) {
+			return "STOP";
+		} else {
+			return "GAZUA";
 		}
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		SW9778 s = new SW9778();
 		int test_case = Integer.parseInt(br.readLine());
-		for(int i = 0; i < test_case; i++) {
+		for(int i = 1; i <= test_case; i++) {
 			int sum = 0;
 			int n = Integer.parseInt(br.readLine());
 			int[] nums = new int[n];
@@ -41,7 +51,10 @@ public class SW9778 {
 				nums[j] = Integer.parseInt(br.readLine());
 				sum += nums[j];
 			}
-			
+			bw.write("#" + i + " " + s.canContinue(nums, sum) + "\n");
 		}
+		br.close();
+		bw.flush();
+		bw.close();
 	}
 }
