057a93a68c5b76248e36577bec7c2d3060297def
OhTaeHo-97 <dhxogh123123@naver.com>
2022-06-27 23:49:27 +0900

    baekjoon_exercise_22/06/27

--- Baekjun/src/baekjun/baekjun13398.java
+++ Baekjun/src/baekjun/baekjun13398.java
@@ -5,26 +5,37 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class baekjun13398 {
+//	public int getMaxSum(int[] nums) {
+//		int[] dp_left = new int[nums.length];
+//		int[] dp_right = new int[nums.length];
+//		dp_left[0] = nums[0];
+//		int max = nums[0];
+//		for(int i = 1; i < nums.length; i++) {
+//			dp_left[i] = Math.max(dp_left[i - 1] + nums[i], nums[i]);
+//			max = Math.max(max, dp_left[i]);
+//		}
+//		dp_right[nums.length - 1] = nums[nums.length - 1];
+//		for(int i = nums.length - 2; i >= 0; i--) {
+//			dp_right[i] = Math.max(dp_right[i + 1] + nums[i], nums[i]);
+//		}
+//		for(int i = 1; i < nums.length - 1; i++) {
+//			int temp = dp_left[i - 1] + dp_right[i + 1];
+//			max = Math.max(max, temp);
+//		}
+//		return max;
+//	}
+	
 	public int getMaxSum(int[] nums) {
-		int[] dp_left = new int[nums.length];
-		int[] dp_right = new int[nums.length];
-		dp_left[0] = nums[0];
+		int[][] dp = new int[nums.length][2];
+		dp[0][0] = nums[0];
+		dp[0][1] = nums[0];
 		int max = nums[0];
 		for(int i = 1; i < nums.length; i++) {
-			dp_left[i] = Math.max(dp_left[i - 1] + nums[i], nums[i]);
-			max = Math.max(max, dp_left[i]);
-		}
-		dp_right[nums.length - 1] = nums[nums.length - 1];
-		for(int i = nums.length - 2; i >= 0; i--) {
-			dp_right[i] = Math.max(dp_right[i + 1] + nums[i], nums[i]);
-		}
-		for(int i = 1; i < nums.length - 1; i++) {
-			int temp = dp_left[i - 1] + dp_right[i + 1];
-			max = Math.max(max, temp);
+			dp[i][0] = Math.max(dp[i - 1][0] + nums[i], nums[i]);
+			dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1] + nums[i]);
+			max = Math.max(max, Math.max(dp[i][0], dp[i][1]));
 		}
 		return max;
 	}
@@ -34,14 +45,10 @@ public class baekjun13398 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int num = Integer.parseInt(br.readLine());
 		int[] nums = new int[num];
-//		ArrayList<Integer> negative = new ArrayList<Integer>();
 		String[] input = br.readLine().split(" ");
 		br.close();
 		for(int i = 0; i < num; i++) {
 			nums[i] = Integer.parseInt(input[i]);
-//			if(nums[i] < 0) {
-//				negative.add(i);
-//			}
 		}
 		baekjun13398 b = new baekjun13398();
 		bw.write(b.getMaxSum(nums) + "\n");
