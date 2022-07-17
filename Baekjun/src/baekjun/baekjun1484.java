2687dda07dcf695b56f98fc6938e2563f0f5cb92
OhTaeHo-97 <dhxogh123123@naver.com>
2022-07-17 13:53:46 +0900

    baekjoon_exercise_22/07/17

---
+++ Baekjun/src/baekjun/baekjun1484.java
@@ -0,0 +1,21 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class baekjun1484 {
+	public void findAllPossibleWeight(int G) {
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int G = Integer.parseInt(br.readLine());
+		br.close();
+		
+	}
+}
