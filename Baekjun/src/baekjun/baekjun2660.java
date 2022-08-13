91eb1fb83b0fc8135f6b64b30de812b99de32581
OhTaeHo-97 <dhxogh123123@naver.com>
2022-08-14 00:14:40 +0900

    baekjoon_exercise_22/08/13

--- Baekjun/src/baekjun/baekjun2660.java
+++ Baekjun/src/baekjun/baekjun2660.java
@@ -4,72 +4,80 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class baekjun2660 {
-	static int n, score;
-	static boolean[] visited;
-	static HashMap<Integer, ArrayList<Integer>> friends;
+	static StringBuilder sb = new StringBuilder();
+	static int n;
+	static int[][] distance;
+	static final int INF = 1000000000;
 	
 	static void input() {
 		Reader scanner = new Reader();
-		friends = new HashMap<>();
 		n = scanner.nextInt();
+		distance = new int[n + 1][n + 1];
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= n; j++) {
+				if(i != j) {
+					distance[i][j] = INF;
+				}
+			}
+		}
 		int n1, n2;
 		n1 = scanner.nextInt();
 		n2 = scanner.nextInt();
 		while(n1 != -1 && n2 != -1) {
-			if(friends.containsKey(n1)) {
-				friends.get(n1).add(n2);
-			} else {
-				friends.put(n1, new ArrayList<Integer>(Arrays.asList(n2)));
-			}
-			if(friends.containsKey(n2)) {
-				friends.get(n2).add(n1);
-			} else {
-				friends.put(n2, new ArrayList<Integer>(Arrays.asList(n1)));
-			}
+			distance[n1][n2] = 1;
+			distance[n2][n1] = 1;
 			n1 = scanner.nextInt();
 			n2 = scanner.nextInt();
 		}
 	}
 	
-	static void bfs(int start) {
-		Queue<Integer> queue = new LinkedList<Integer>();
-		visited[start] = true;
-		for(int friend : friends.get(start)) {
-			queue.offer(friend);
-		}
-		int size = queue.size();
-		while(!queue.isEmpty()) {
-			int cur_position = queue.poll();
-			if(!visited[cur_position]) {
-				visited[cur_position] = true;
-				for(int friend : friends.get(cur_position)) {
-					if(!visited[friend])
-						queue.offer(friend);
+	static void FloydWarshall() { // 플로이드 와샬 알고리즘
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= n; j++) {
+				for(int k = 1; k <= n; k++) {
+					if(distance[j][k] > distance[j][i] + distance[i][k]) {
+						distance[j][k] = distance[j][i] + distance[i][k];
+					}
 				}
-				score++;
 			}
 		}
 	}
 	
 	static void solution() {
+		FloydWarshall();
+		int[] each_distance = new int[n + 1];
+		int min = INF;
 		for(int i = 1; i <= n; i++) {
-			visited = new boolean[n + 1];
-			score = 0;
-			bfs(i);
-			System.out.println(i + ": " + score);
+			int dis = 0;
+			for(int j = 1; j <= n; j++) {
+				if(distance[i][j] != INF) {
+					dis = Math.max(dis, distance[i][j]);
+				}
+			}
+			each_distance[i] = dis;
+			min = Math.min(min, dis);
 		}
+		ArrayList<Integer> candidate = new ArrayList<Integer>();
+		for(int i = 1; i <= n; i++) {
+			if(min == each_distance[i]) {
+				candidate.add(i);
+			}
+		}
+		sb.append(min + " " + candidate.size());
+		sb.append("\n");
+		for(int c : candidate) {
+			sb.append(c + " ");
+		}
+		sb.append("\n");
 	}
 	
 	public static void main(String[] args) {
 		input();
 		solution();
+		System.out.println(sb.toString());
 	}
 	
 	static class Reader {
@@ -91,14 +99,6 @@ public class baekjun2660 {
 		}
 		int nextInt() {
 			return Integer.parseInt(next());
-		}
-	}
-	
-	static class Edge {
-		int node, weight;
-		public Edge(int node, int weight) {
-			this.node = node;
-			this.weight = weight;
 		}
 	}
 }
