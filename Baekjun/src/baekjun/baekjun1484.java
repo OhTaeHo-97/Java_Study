835df26866336a3790947340bd9b44a09b759830
OhTaeHo-97 <dhxogh123123@naver.com>
2022-08-17 22:39:10 +0900

    baekjoon_exercise_22/08/17(edit)

--- Baekjun/src/baekjun/baekjun1484.java
+++ Baekjun/src/baekjun/baekjun1484.java
@@ -1,57 +1,68 @@
 package baekjun;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
 
-// 해당 문제는 블로그 게시 X
 public class baekjun1484 {
-	public ArrayList<Integer> findAllPossibleWeight(int G) {
-		long[] squared_nums = new long[100001];
-		for(int i = 1; i < squared_nums.length; i++) {
-			squared_nums[i] = i * i;
-		}
-		ArrayList<Integer> result = new ArrayList<Integer>();
-		int left = 1;
-		int right = 2;
-		while(right <= 100000) {
-			long dif = squared_nums[right] - squared_nums[left];
-			if(dif == G) {
-				result.add(right);
-			}
+	static StringBuilder sb = new StringBuilder();
+	static int G;
+	
+	static void input() {
+		Reader scanner = new Reader();
+		G = scanner.nextInt();
+	}
+	
+	static void solution() {
+		TreeSet<Integer> cur_weight = new TreeSet<>();
+		int l = 1, r = 1;
+		while(r <= 100000) {
+			long dif = (long)r * r - (long)l * l;
 			if(dif > G) {
-				left++;
-			}
-			if(dif <= G) {
-				right++;
+				l++;
+			} else {
+				if(dif == G) {
+					cur_weight.add(r);
+				}
+				r++;
 			}
 		}
-		if(result.isEmpty() || G == 1) {
-			return null;
-		} else {
-			Collections.sort(result);
-			return result;
+		if(cur_weight.size() == 0) {
+			sb.append(-1 + "\n");
+		} else {			
+			for(int w : cur_weight) {
+				sb.append(w + "\n");
+			}
 		}
 	}
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int G = Integer.parseInt(br.readLine());
-		br.close();
-		baekjun1484 b = new baekjun1484();
-		if(b.findAllPossibleWeight(G) == null)
-			bw.write(-1 + "\n");
-		else {
-			ArrayList<Integer> result = b.findAllPossibleWeight(G);
-			for(int r : result)
-				bw.write(r + "\n");
+	public static void main(String[] args) {
+		input();
+		solution();
+		System.out.println(sb.toString());
+	}
+	
+	static class Reader {
+		BufferedReader br;
+		StringTokenizer st;
+		public Reader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
 		}
-		bw.flush();
-		bw.close();
+		String next() {
+			while(st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
 	}
 }
