bd33cfe65a3ebcb6acf0fb41985f1f375d6715dc
OhTaeHo-97 <dhxogh123123@naver.com>
2022-10-12 19:01:23 +0900

    baekjoon_exercise_22/10/01

--- Baekjun/src/baekjun/baekjun14890.java
+++ Baekjun/src/baekjun/baekjun14890.java
@@ -23,16 +23,24 @@ public class baekjun14890 {
 	static void solution() {
 		int answer = 0;
 		for(int row = 0; row < N; row++) {
-			if(isRoad(row)) {
+			if(isRowRoad(row)) {
 				answer++;
 			} else {
-				if(checkColumns(row)) answer++;
+				if(checkRows(row)) answer++;
 			}
 		}
-		
+		slopeWay = new boolean[N][N];
+		for(int col = 0; col < N; col++) {
+			if(isColRoad(col)) {
+				answer++;
+			} else {
+				if(checkColumns(col)) answer++;
+			}
+		}
+		System.out.println(answer);
 	}
 	
-	static boolean isRoad(int row) {
+	static boolean isRowRoad(int row) {
 		int num = map[row][0];
 		for(int col = 1; col < N; col++) {
 			if(map[row][col] != num) return false;
@@ -40,7 +48,15 @@ public class baekjun14890 {
 		return true;
 	}
 	
-	static boolean checkColumns(int row) {
+	static boolean isColRoad(int col) {
+		int num = map[0][col];
+		for(int row = 1; row < N; row++) {
+			if(map[row][col] != num) return false;
+		}
+		return true;
+	}
+	
+	static boolean checkRows(int row) {
 		int num = map[row][0];
 		for(int col = 1; col < N; col++) {
 			if(num == map[row][col]) continue;
@@ -52,20 +68,17 @@ public class baekjun14890 {
 					if(slopeWay[row][col2]) return false;
 					slopeWay[row][col2] = true;
 					if(num != map[row][col2]) {
-						for(int col3 = col; col3 <= col + L - 1; col3++)
-							slopeWay[row][col3] = false;
 						return false;
 					}
 				}
 			} else if(num < map[row][col]) { // 높이가 높아졌을 때
 				if(map[row][col] > num + 1) return false;
 				if(col - L < 0) return false;
+				num = map[row][col - 1];
 				for(int col2 = col - L; col2 < col; col2++) {
 					if(slopeWay[row][col2]) return false;
 					slopeWay[row][col2] = true;
 					if(num != map[row][col2]) {
-						for(int col3 = col - L; col3 < col; col3++)
-							slopeWay[row][col3] = false;
 						return false;
 					}
 				}
@@ -75,32 +88,29 @@ public class baekjun14890 {
 		return true;
 	}
 	
-	static boolean checkRows(int col) {
+	static boolean checkColumns(int col) {
 		int num = map[0][col];
 		for(int row = 1; row < N; row++) {
 			if(num == map[row][col]) continue;
 			if(num > map[row][col]) { // 높이가 낮아졌을 때
 				if(map[row][col] < num - 1) return false;
-				if(col + L - 1 >= N) return false;
+				if(row + L - 1 >= N) return false;
 				num = map[row][col];
-				for(int col2 = col; col2 <= col + L - 1; col2++) {
-					if(slopeWay[row][col2]) return false;
-					slopeWay[row][col2] = true;
-					if(num != map[row][col2]) {
-						for(int col3 = col; col3 <= col + L - 1; col3++)
-							slopeWay[row][col3] = false;
+				for(int row2 = row; row2 <= row + L - 1; row2++) {
+					if(slopeWay[row2][col]) return false;
+					slopeWay[row2][col] = true;
+					if(num != map[row2][col]) {
 						return false;
 					}
 				}
 			} else if(num < map[row][col]) { // 높이가 높아졌을 때
 				if(map[row][col] > num + 1) return false;
-				if(col - L < 0) return false;
-				for(int col2 = col - L; col2 < col; col2++) {
-					if(slopeWay[row][col2]) return false;
-					slopeWay[row][col2] = true;
-					if(num != map[row][col2]) {
-						for(int col3 = col - L; col3 < col; col3++)
-							slopeWay[row][col3] = false;
+				if(row - L < 0) return false;
+				num = map[row - 1][col];
+				for(int row2 = row - L; row2 < row; row2++) {
+					if(slopeWay[row2][col]) return false;
+					slopeWay[row2][col] = true;
+					if(num != map[row2][col]) {
 						return false;
 					}
 				}
