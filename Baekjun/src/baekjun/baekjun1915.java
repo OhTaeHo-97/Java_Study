1918f94058f730e06f85c924bea2727f697ad8e1
OhTaeHo-97 <dhxogh123123@naver.com>
2022-10-16 23:22:34 +0900

    baekjoon_exercise_22/10/15

--- Baekjun/src/baekjun/baekjun1915.java
+++ Baekjun/src/baekjun/baekjun1915.java
@@ -6,8 +6,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class baekjun1915 {
+	static int n, m, result;
+	static int[][] matrix, dp;
+	static void input() {
+		Reader scanner = new Reader();
+		n = scanner.nextInt();
+		m = scanner.nextInt();
+		matrix = new int[n][m];
+		dp = new int[n][m];
+		result = 0;
+		for(int row = 0; row < n; row++) {
+			String info = scanner.nextLine();
+			for(int col = 0; col < m; col++) {
+				matrix[row][col] = info.charAt(col) - '0';
+				dp[row][col] = matrix[row][col];
+				if(dp[row][col] == 1) result = 1;
+			}
+		}
+	}
+	
+	static void solution() {
+		for(int row = 1; row < n; row++) {
+			for(int col = 1; col < m; col++) {
+				if(matrix[row][col] == 1) {
+					dp[row][col] = Math.min(dp[row][col - 1], Math.min(dp[row - 1][col], dp[row - 1][col - 1])) + 1;
+					result = dp[row][col] > result ? dp[row][col] : result;
+				}
+			}
+		}
+		System.out.println(result * result);
+	}
+	
 	public static void main(String[] args) {
-		
+		input();
+		solution();
 	}
 	
 	static class Reader {
@@ -28,6 +60,15 @@ public class baekjun1915 {
 		}
 		int nextInt() {
 			return Integer.parseInt(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			return str;
 		}
 	}
 }
