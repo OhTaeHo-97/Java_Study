a6ff594dd29b4957a0512c0fc2fa4d5bfd28e7eb
OhTaeHo-97 <dhxogh123123@naver.com>
2022-11-11 21:24:21 +0900

    baekjoon_exercise_22/11/10

--- Baekjun/src/baekjun/baekjun4179.java
+++ Baekjun/src/baekjun/baekjun4179.java
@@ -3,7 +3,6 @@ package baekjun;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -12,44 +11,42 @@ public class baekjun4179 {
 	static int R, C;
 	static char[][] map;
 	static int[] start, dx = {-1, 0, 1, 0}, dy = {0, -1, 0, 1};
-	static int[][] times, fireTimes;
+	static int[][] times;
 	static LinkedList<int[]> fires;
 	static void input() {
 		Reader scanner = new Reader();
 		R = scanner.nextInt();
 		C = scanner.nextInt();
 		map = new char[R][C];
+		times = new int[R][C];
 		fires = new LinkedList<>();
 		for(int row = 0; row < R; row++) {
 			String info = scanner.nextLine();
 			for(int col = 0; col < C; col++) {
 				map[row][col] = info.charAt(col);
+				times[row][col] = Integer.MAX_VALUE;
 				if(map[row][col] == 'J') {
 					start = new int[] {row, col};
 					map[row][col] = '.';
-				}
-				else if(map[row][col] == 'F') fires.add(new int[] {row, col});
+					times[row][col] = 0;
+				} else if(map[row][col] == 'F') fires.add(new int[] {row, col});	
 			}
 		}
 	}
 	
 	static void solution() {
-		times = new int[R][C];
-		for(int row = 0; row < R; row++) Arrays.fill(times[row], Integer.MAX_VALUE);
-		times[start[0]][start[1]] = 0;
-		fireTimes = new int[R][C];
 		Queue<int[]> loc = new LinkedList<>();
 		loc.offer(start);
 		int time = 0;
 		while(!loc.isEmpty()) {
 			int[] cur = loc.poll();
-			if(time == times[cur[0]][cur[1]]) {
+			if(times[cur[0]][cur[1]] == time) {
 				spreadFire();
 				time++;
 			}
 			for(int dir = 0; dir < 4; dir++) {
 				int cx = cur[0] + dx[dir], cy = cur[1] + dy[dir];
-				if(cx == R || cy == C) {
+				if(cx < 0 || cx >= R ||  cy < 0 || cy >= C) {
 					System.out.println(times[cur[0]][cur[1]] + 1);
 					return;
 				}
@@ -65,33 +62,27 @@ public class baekjun4179 {
 	}
 	
 	static void spreadFire() {
-		int time = times[fires.get(0)[0]][fires.get(0)[1]], size = fires.size();
-		Queue<int[]> fireLoc = new LinkedList<int[]>();
-		for(int index = 0; index < size; index++) {
-			fireLoc.offer(fires.get(index));
-			fireLoc.remove(0);
-		}
-		while(!fireLoc.isEmpty()) {
-			int[] cur = fireLoc.poll();
-			if(fireTimes[cur[0]][cur[1]] > time) return;
+		int size = fires.size();
+		for(int count = 0; count < size; count++) {
+			int[] cur = fires.get(0);
+			fires.remove(0);
 			for(int dir = 0; dir < 4; dir++) {
 				int cx = cur[0] + dx[dir], cy = cur[1] + dy[dir];
 				if(isInMap(cx, cy)) {
 					if(map[cx][cy] == '.') {
-						fireLoc.offer(new int[] {cx, cy});
 						fires.add(new int[] {cx, cy});
-						fireTimes[cx][cy] = fireTimes[cur[0]][cur[1]] + 1;
+						map[cx][cy] = 'F';
 					}
 				}
 			}
 		}
 	}
-	
+   
 	static boolean isInMap(int x, int y) {
 		if(x >= 0 && x < R && y >= 0 && y < C) return true;
 		return false;
 	}
-	
+   
 	public static void main(String[] args) {
 		input();
 		solution();
