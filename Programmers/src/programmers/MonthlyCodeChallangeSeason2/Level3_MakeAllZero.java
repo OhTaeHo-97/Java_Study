e49861495ed3c722e4ccc57a6a58539826b36e45
OhTaeHo-97 <dhxogh123123@naver.com>
2022-12-02 00:20:00 +0900

    programmers_exercise_22/12/01

--- Programmers/src/programmers/MonthlyCodeChallangeSeason2/Level3_MakeAllZero.java
+++ Programmers/src/programmers/MonthlyCodeChallangeSeason2/Level3_MakeAllZero.java
@@ -5,10 +5,9 @@ import java.util.*;
 public class Level3_MakeAllZero {
 	static HashMap<Integer, ArrayList<Integer>> tree;
 	static long[] weights;
-	static int size;
+	static int[] indegree;
+	static int size, root;
 	static long answer;
-	static HashSet<Integer> visited;
-	static ArrayList<Integer> leafs;
 	public static long solution(int[] a, int[][] edges) {
 		long sum = 0L;
 		boolean isZero = true;
@@ -18,47 +17,49 @@ public class Level3_MakeAllZero {
 		}
 		if(sum != 0) return -1;
 		if(isZero) return 0;
+		if(a.length == 2) return Math.abs(a[0]);
+		
 		size = a.length;
-		weights = new long[a.length];
+		weights = new long[size];
 		for(int idx = 0; idx < a.length; idx++) weights[idx] = a[idx];
 		answer = 0L;
-		visited = new HashSet<>();
 		makeTree(edges);
-		findLeaf();
-		calc(leafs);
+		bfs();
 		return answer;
 	}
 	
 	static void makeTree(int[][] edges) {
 		tree = new HashMap<>();
+		indegree = new int[size];
 		for(int node = 0; node < size; node++) tree.put(node, new ArrayList<>());
 		for(int[] edge : edges) {
 			tree.get(edge[0]).add(edge[1]);
 			tree.get(edge[1]).add(edge[0]);
+			indegree[edge[0]]++;
+			indegree[edge[1]]++;
 		}
 	}
 	
-	static void findLeaf() {
-		leafs = new ArrayList<>();
-		for(int node = 0; node < size; node++) {
-			if(tree.get(node).size() == 1) leafs.add(node);
+	static void bfs() {
+		Queue<Integer> queue = new LinkedList<>();
+		for(int node = 1; node < size; node++) {
+			if(tree.get(node).size() == 1) queue.offer(node);
 		}
-	}
-	
-	static void calc(ArrayList<Integer> list) {
-		if(list.size() == 0 || (list.size() == 1 && visited.contains(list.get(0)))) return;
-		ArrayList<Integer> temp = new ArrayList<>();
-		for(int node : list) {
-			if(!visited.add(node)) continue;
-			for(int neighbor : tree.get(node)) {
-				if(!visited.contains(neighbor)) {
-					weights[neighbor] += weights[node];
-					answer += Math.abs(weights[node]);
-					temp.add(neighbor);
+		while(!queue.isEmpty()) {
+			int cur = queue.poll();
+			answer += Math.abs(weights[cur]);
+			indegree[cur]--;
+			for(int idx = 0; idx < tree.get(cur).size(); idx++) {
+				int next = tree.get(cur).get(idx);
+				if(indegree[next] == 0) continue;
+				indegree[next]--;
+				weights[next] += weights[cur];
+				if(indegree[next] == 1) {
+					if(next == root) continue;
+					queue.offer(next);
 				}
 			}
 		}
-		calc(temp);
 	}
 	
 	public static void main(String[] args) {
