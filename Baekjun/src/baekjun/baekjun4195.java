d5cddc569d20b73ebec9bb9810dea8f60942a1a0
OhTaeHo-97 <dhxogh123123@naver.com>
2023-01-29 16:21:21 +0900

    baekjoon_exercise_23/01/28

--- Baekjun/src/baekjun/baekjun4195.java
+++ Baekjun/src/baekjun/baekjun4195.java
@@ -2,79 +2,64 @@ package baekjun;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class baekjun4195 {
 	static StringBuilder sb = new StringBuilder();
     static Reader scanner = new Reader();
     static int F;
-    static HashMap<String, String> parents;
-    static HashMap<String, HashSet<String>> children;
+    static HashMap<String, Integer> people;
+    static int[] parents, children;
     static void input() {
         F = scanner.nextInt();
-        parents = new HashMap<>();
-        children = new HashMap<>();
+        people = new HashMap<>();
+        parents = new int[F * 2];
+        children = new int[F * 2];
+        int idx = 0;
         for(int relation = 0; relation < F; relation++) {
             String person1 = scanner.next(), person2 = scanner.next();
-            if(person1.equals(person2)) {
-                if(!parents.containsKey(person1))
-                    parents.put(person1, person1);
+            if(!people.containsKey(person1))
+                idx = addPerson(idx, person1);
+            if(!people.containsKey(person2))
+                idx = addPerson(idx, person2);
 
-                if(!children.containsKey(person1))
-                    children.put(person1, new HashSet<>(Arrays.asList(person1)));
-            } else {
-                addElementInParents(person1);
-                addElementInParents(person2);
+            sb.append(union(people.get(person1), people.get(person2))).append('\n');
+        }
+    }
 
-                union(person1, person2);
+    static int addPerson(int idx, String person) {
+        people.put(person, idx++);
+        parents[people.get(person)] = people.get(person);
+        children[people.get(person)]++;
+        return idx;
+    }
+
+    static int findParent(int person) {
+        if(person == parents[person]) return person;
+        return parents[person] = findParent(parents[person]);
+    }
+
+    static int union(int person1, int person2) {
+        int parent1 = findParent(person1), parent2 = findParent(person2);
+        int result = children[parent1];
+
+        if(parent1 != parent2) {
+            if(parent1 < parent2) {
+                parents[parent2] = parent1;
+                children[parent1] += children[parent2];
+                children[parent2] = 1;
+                result = children[parent1];
             }
-
-            String parent = findParent(person1);
-            parent = parents.get(person1);
-
-            sb.append(children.get(parent).size()).append('\n');
+            else {
+                parents[parent1] = parent2;
+                children[parent2] += children[parent1];
+                children[parent1] = 1;
+                result = children[parent2];
+            }
         }
-    }
 
-    static void addElementInParents(String person) {
-        if(!parents.containsKey(person)) parents.put(person, person);
-    }
-
-    static String findParent(String person) {
-        if(person.equals(parents.get(person))) return person;
-        return parents.put(person, findParent(parents.get(person)));
-    }
-
-    static void union(String person1, String person2) {
-        String parent1 = findParent(person1), parent2 = findParent(person2);
-        parent1 = parents.get(person1);
-        parent2 = parents.get(person2);
-
-        if(!parent1.equals(parent2)) {
-            if(parent1.compareTo(parent2) < 0)
-                editChildren(parent2, parent1);
-            else
-                editChildren(parent1, parent2);
-        }
-    }
-
-    private static void editChildren(String child, String parent) {
-        parents.put(child, parent);
-
-        if(children.getOrDefault(parent, null) == null)
-            children.put(parent, new HashSet<>(Arrays.asList(parent)));
-
-        if(child == parent) return;
-
-        children.get(parent).add(child);
-
-        if(children.getOrDefault(child, null) != null) {
-            children.get(parent).addAll(children.get(child));
-            children.put(child, null);
-        }
+        return result;
     }
 
     public static void main(String[] args) {
