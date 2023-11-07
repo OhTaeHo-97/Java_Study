2e20f9dc72961faaa67b5cf84f1f7c354ba7964f
otaeho <dhxogh123123@naver.com>
2023-11-07 22:31:33 +0900

    baekjoon_exercise_23/11/07

--- Baekjun/src/baekjun/baekjun21757.java
+++ Baekjun/src/baekjun/baekjun21757.java
@@ -6,22 +6,44 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class baekjun21757 {
-    static int N;
-    static long[] sums;
+    static int seriesCount;
+    static int[] series;
+    static int[] prefixSum;
 
     static void input() {
         Reader scanner = new Reader();
 
-        N = scanner.nextInt();
-        sums = new long[N + 1];
-        for(int idx = 1; idx <= N; idx++) {
-            sums[idx] = scanner.nextInt();
-            sums[idx] += sums[idx - 1];
+        seriesCount = scanner.nextInt();
+        series = new int[seriesCount + 1];
+        prefixSum = new int[seriesCount + 1];
+
+        for(int idx = 1; idx <= seriesCount; idx++) {
+            series[idx] = scanner.nextInt();
+            prefixSum[idx] = prefixSum[idx - 1] + series[idx];
         }
     }
 
     static void solution() {
+        if(prefixSum[seriesCount] % 4 != 0) {
+            System.out.println(0);
+            return;
+        }
 
+        int diff = prefixSum[seriesCount] / 4;
+        // dp[i][j] = i번째까지 j번으로 나누는 경우의 수
+        long[][] dp = new long[seriesCount + 1][5];
+        dp[0][0] = 1;
+        for(int idx = 1; idx <= seriesCount; idx++) {
+            dp[idx][0] = 1;
+            for(int idx2 = 1; idx2 < 4; idx2++) {
+                dp[idx][idx2] = dp[idx - 1][idx2];
+                if(diff * idx2 == prefixSum[idx]) {
+                    dp[idx][idx2] += dp[idx - 1][idx2 - 1];
+                }
+            }
+        }
+
+        System.out.println(dp[seriesCount - 1][3]);
     }
 
     public static void main(String[] args) {
@@ -38,7 +60,7 @@ public class baekjun21757 {
         }
 
         String next() {
-            while(st == null || !st.hasMoreElements()) {
+            while (st == null || !st.hasMoreElements()) {
                 try {
                     st = new StringTokenizer(br.readLine());
                 } catch (IOException e) {
