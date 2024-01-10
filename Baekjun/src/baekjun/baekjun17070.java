b1881dc108645e2aec5c5b5d9d609710a6c503c6
otaeho <dhxogh123123@naver.com>
2024-01-10 20:51:39 +0900

    baekjoon_exercise_24/01/09

--- Baekjun/src/baekjun/baekjun17070.java
+++ Baekjun/src/baekjun/baekjun17070.java
@@ -7,35 +7,52 @@ import java.util.StringTokenizer;
 
 public class baekjun17070 {
     static int houseSize;
-    static int[][] map;
+    static int answer;
+    static boolean[][] map;
 
     static void input() {
         Reader scanner = new Reader();
 
         houseSize = scanner.nextInt();
-        map = new int[houseSize][houseSize];
+        answer = 0;
+        map = new boolean[houseSize][houseSize];
 
-        for(int row = 0; row < houseSize; row++) {
-            for(int col = 0; col < houseSize; col++) {
-                map[row][col] = scanner.nextInt();
+        for (int row = 0; row < houseSize; row++) {
+            for (int col = 0; col < houseSize; col++) {
+                int info = scanner.nextInt();
+                map[row][col] = (info == 0);
             }
         }
     }
 
     static void solution() {
-
+        dfs(0, 1, 0);
+        System.out.println(answer);
     }
 
-    static class Pipe {
-        int[] beforePoint;
-        int[] nextPoint;
-        int direction;
-
-        public Pipe(int[] beforePoint, int[] nextPoint, int direction) {
-            this.beforePoint = beforePoint;
-            this.nextPoint = nextPoint;
-            this.direction = direction;
+    static void dfs(int x, int y, int direction) {
+        if (x == houseSize - 1 && y == houseSize - 1) {
+            answer++;
+            return;
         }
+
+        if (direction == 0 || direction == 1) {
+            if (y + 1 < houseSize && map[x][y + 1]) {
+                dfs(x, y + 1, 0);
+            }
+        }
+        if (direction == 1 || direction == 2) {
+            if (x + 1 < houseSize && map[x + 1][y]) {
+                dfs(x + 1, y, 2);
+            }
+        }
+        if (x + 1 < houseSize && y + 1 < houseSize && isBlank(x, y)) {
+            dfs(x + 1, y + 1, 1);
+        }
+    }
+
+    static boolean isBlank(int x, int y) {
+        return map[x][y + 1] && map[x + 1][y] && map[x + 1][y + 1];
     }
 
     public static void main(String[] args) {
@@ -52,7 +69,7 @@ public class baekjun17070 {
         }
 
         String next() {
-            while(st == null || !st.hasMoreElements()) {
+            while (st == null || !st.hasMoreElements()) {
                 try {
                     st = new StringTokenizer(br.readLine());
                 } catch (IOException e) {
