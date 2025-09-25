f1a9e971c58af3af4bddd2e0a9fee4d87ebbbd6c
otaeho <dhxogh123123@naver.com>
2025-09-25 21:58:56 +0900

    baekjoon_exercise_25/09/25

--- Baekjun/src/baekjun/baekjun15898.java
+++ Baekjun/src/baekjun/baekjun15898.java
@@ -1,176 +1,251 @@
-package src.baekjun;
+package baekjun;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class baekjun15898 {
-    static int n, answer;
-    static Material[][] map;
-    static Material[][][] materials;
-    static int[] candidates;
-    static int[][] startIdx;
-    static boolean[] visited;
+    private static final int SIZE = 5;
+    private static final int SELECT_SIZE = 3;
+    private static final int MATERIAL_SIZE = 4;
+    private static Map<Character, Integer> points = new HashMap<>() {{
+        put('R', 7);
+        put('B', 5);
+        put('G', 3);
+        put('Y', 2);
+        put('W', 0);
+    }};
 
-    static void input() {
+    private static int answer;
+    private static int materialCount;
+    private static Material[][][] materials;
+    private static int[] selectedMaterials;
+    private static int[] rotateDirections;
+    private static boolean[] visited;
+    private static Material[][] gama;
+
+    private static void input() {
         Reader scanner = new Reader();
 
-        n = scanner.nextInt();
-        map = new Material[5][5];
-        for(int row = 0; row < map.length; row++)
-            Arrays.fill(map[row], new Material(0, 'W'));
+        materialCount = scanner.nextInt();
+        materials = new Material[materialCount][MATERIAL_SIZE][MATERIAL_SIZE];
+        selectedMaterials = new int[SELECT_SIZE];
+        rotateDirections = new int[SELECT_SIZE];
+        visited = new boolean[materialCount];
+        gama = new Material[SIZE][SIZE];
 
-        materials = new Material[n][4][4];
-        for(int material = 0; material < n; material++) {
-            // 효능 입력
-            for(int row = 0; row < materials[material].length; row++) {
-                for(int col = 0; col < materials[material][row].length; col++)
-                    materials[material][row][col] = new Material(scanner.nextInt());
-            }
-
-            // 원소 입력
-            for(int row = 0; row < materials[material].length; row++) {
-                for(int col = 0; col < materials[material][row].length; col++)
-                    materials[material][row][col].element = scanner.next().charAt(0);
+        for(int row = 0; row < SIZE; row++) {
+            for(int col = 0; col < SIZE; col++) {
+                gama[row][col] = new Material();
             }
         }
 
-        candidates = new int[3];
-        startIdx = new int[3][2];
-        visited = new boolean[n];
+        for(int count = 0; count < materialCount; count++) {
+            for(int row = 0; row < MATERIAL_SIZE; row++) {
+                for(int col = 0; col < MATERIAL_SIZE; col++) {
+                    int quality = scanner.nextInt();
+                    materials[count][row][col] = new Material(quality);
+                }
+            }
+            for(int row = 0; row < MATERIAL_SIZE; row++) {
+                for(int col = 0; col < MATERIAL_SIZE; col++) {
+                    materials[count][row][col].color = scanner.next().charAt(0);
+                }
+            }
+        }
     }
 
-    static void solution() {
-        setMaterialAndOrder(0);
+    private static void solution() {
+        selectMaterial(0);
         System.out.println(answer);
     }
 
-    static void setMaterialAndOrder(int index) {
-        if(index == 3) {
-            int quality = simulation();
-            answer = Math.max(answer, quality);
+    private static void selectMaterial(int selectedCount) {
+        if(selectedCount >= SELECT_SIZE) {
+//            simulation(0, gama);
+            simulation(0, new MaterialInfo[SELECT_SIZE]);
             return;
         }
 
-        for(int idx = 0; idx < n; idx++) {
-            if(!visited[idx]) {
-                visited[idx] = true;
-                candidates[index] = idx;
+        for(int idx = 0; idx < materialCount; idx++) {
+            if(visited[idx]) continue;
 
-                for(int dir = 0; dir < 4; dir++) {
-                    for(int row = 0; row <= map.length - materials.length; row++) {
-                        for(int col = 0; col <= map[0].length - materials[0].length; col++) {
-                            startIdx[index] = new int[] {row, col};
-                            setMaterialAndOrder(index + 1);
-                        }
-                    }
+            selectedMaterials[selectedCount] = idx;
+            visited[idx] = true;
+            selectMaterial(selectedCount + 1);
+            selectedMaterials[selectedCount] = 0;
+            visited[idx] = false;
+        }
+    }
 
-                    materials[idx] = rotate(materials[idx]);
+//    private static void simulation(int materialIdx, Material[][] gama) {
+//        if(materialIdx >= selectedMaterials.length) {
+//            answer = Math.max(answer, calculateQuality(gama));
+//            return;
+//        }
+//
+//        for(int row = 0; row <= SIZE - MATERIAL_SIZE; row++) {
+//            for(int col = 0; col <= SIZE - MATERIAL_SIZE; col++) {
+//                Material[][] material = materials[selectedMaterials[materialIdx]];
+//                for(int rotateCount = 0; rotateCount < 4; rotateCount++) {
+//                    Material[][] copy = copyGama(gama);
+//                    if(rotateCount == 0) {
+//                        putMaterial(row, col, material, copy);
+//                        simulation(materialIdx + 1, copy);
+//                    } else {
+//                        material = rotate90(material);
+//                        putMaterial(row, col, material, copy);
+//                        simulation(materialIdx + 1, copy);
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+    private static void simulation(int materialIdx, MaterialInfo[] materialInfos) {
+        if(materialIdx >= selectedMaterials.length) {
+            answer = Math.max(answer, calculateQuality(materialInfos));
+            return;
+        }
+
+        for(int row = 0; row <= SIZE - MATERIAL_SIZE; row++) {
+            for(int col = 0; col <= SIZE - MATERIAL_SIZE; col++) {
+                for(int rotateIdx = 0; rotateIdx < 4; rotateIdx++) {
+                    materialInfos[materialIdx] = new MaterialInfo(row, col, selectedMaterials[materialIdx], rotateIdx);
+                    simulation(materialIdx + 1, materialInfos);
+                }
+//                Material[][] material = materials[selectedMaterials[materialIdx]];
+//                for(int rotateCount = 0; rotateCount < 4; rotateCount++) {
+//                    Material[][] copy = copyGama(gama);
+//                    if(rotateCount == 0) {
+//                        putMaterial(row, col, material, copy);
+//                        simulation(materialIdx + 1, copy);
+//                    } else {
+//                        material = rotate90(material);
+//                        putMaterial(row, col, material, copy);
+//                        simulation(materialIdx + 1, copy);
+//                    }
+//                }
+            }
+        }
+    }
+
+//    private static int calculateQuality(Material[][] gama) {
+//        int totalQuality = 0;
+//
+//        for(int row = 0; row < SIZE; row++) {
+//            for(int col = 0; col < SIZE; col++) {
+//                totalQuality += (gama[row][col].quality * points.get(gama[row][col].color));
+//            }
+//        }
+//
+//        return totalQuality;
+//    }
+
+    private static int calculateQuality(MaterialInfo[] materialInfos) {
+        int totalQuality = 0;
+
+        Material[][] gama = new Material[SIZE][SIZE];
+        for(int row = 0; row < SIZE; row++) {
+            for(int col = 0; col < SIZE; col++) {
+                gama[row][col] = new Material();
+            }
+        }
+
+        for(MaterialInfo materialInfo : materialInfos) {
+            if(materialInfo.rotateIdx != 0) {
+                Material[][] material = materials[materialInfo.materialIdx];
+                for(int count = 0; count < materialInfo.rotateIdx; count++) {
+                    material = rotate90(material);
                 }
 
-                visited[idx] = false;
+                putMaterial(materialInfo.x, materialInfo.y, material, gama);
+            }
+        }
+
+        for(int row = 0; row < SIZE; row++) {
+            for(int col = 0; col < SIZE; col++) {
+                totalQuality += (gama[row][col].quality * points.get(gama[row][col].color));
+            }
+        }
+
+        return totalQuality;
+    }
+
+    private static void putMaterial(int x, int y, Material[][] material, Material[][] gama) {
+        for(int row = x; row < x + MATERIAL_SIZE; row++) {
+            for(int col = y; col < y + MATERIAL_SIZE; col++) {
+                int quality = gama[row][col].quality + material[row - x][col - y].quality;
+                if(quality < 0) quality = 0;
+                if(quality > 9) quality = 9;
+                char color = material[row - x][col - y].color == 'W' ? gama[row][col].color : material[row - x][col - y].color;
+
+                gama[row][col].quality = quality;
+                gama[row][col].color = color;
             }
         }
     }
 
-    static void print(Material[][] material) {
-        for(int row = 0; row < material.length; row++)
-            System.out.println(Arrays.toString(material[row]));
-    }
-
-    static int simulation() {
-        Material[][] copy = copy(map);
-
-        for(int idx = 0; idx < candidates.length; idx++) {
-//            print(materials[candidates[idx]]);
-//            System.out.println(Arrays.toString(startIdx[idx]));
-//            System.out.println();
-            change(startIdx[idx][0], startIdx[idx][1], materials[candidates[idx]], copy);
-        }
-
-//        print(copy);
-
-        return calculateQuality(copy);
-    }
-
-    static int calculateQuality(Material[][] map) {
-        int R = 0, B = 0, G = 0, Y = 0;
-
-        for(int row = 0; row < map.length; row++) {
-            for(int col = 0; col < map[row].length; col++) {
-                char element = map[row][col].element;
-
-                if(element == 'R') R += map[row][col].effect;
-                else if(element == 'B') B += map[row][col].effect;
-                else if(element == 'G') G += map[row][col].effect;
-                else if(element == 'Y') Y += map[row][col].effect;
+    private static Material[][] copyGama(Material[][] gama) {
+        Material[][] copyGama = new Material[SIZE][SIZE];
+        for(int row = 0; row < SIZE; row++) {
+            for(int col = 0; col < SIZE; col++) {
+                Material materialElement = gama[row][col];
+                copyGama[row][col] = new Material(materialElement.quality, materialElement.color);
             }
         }
-
-        return 7 * R + 5 * B + 3 * G + 2 * Y;
+        return copyGama;
     }
 
-    static void change(int startX, int startY, Material[][] material, Material[][] map) {
-        for(int row = 0; row < material.length; row++) {
-            for(int col = 0; col < material[row].length; col++) {
-                int effect = map[startX + row][startY + col].effect + material[row][col].effect;
-                if(effect < 0) effect = 0;
-                else if(effect > 9) effect = 9;
-
-                map[startX + row][startY + col].effect = effect;
-
-                if(material[row][col].element != 'W')
-                    map[startX + row][startY + col].element = material[row][col].element;
+    private static Material[][] rotate90(Material[][] material) {
+        Material[][] copy = new Material[MATERIAL_SIZE][MATERIAL_SIZE];
+        for(int count = 0; count < MATERIAL_SIZE / 2; count++) {
+            for(int row = count; row < MATERIAL_SIZE - count; row++) {
+                copy[row][(MATERIAL_SIZE - 1) - count] = new Material(material[0][row].quality, material[0][row].color);
+                copy[row][count] = new Material(material[(MATERIAL_SIZE - 1) - count][row].quality, material[(MATERIAL_SIZE - 1) - count][row].color);
             }
-        }
-    }
-
-    static Material[][] copy(Material[][] map) {
-        Material[][] copy = new Material[map.length][map[0].length];
-        for(int row = 0; row < copy.length; row++) {
-            for(int col = 0; col < copy[row].length; col++)
-                copy[row][col] = map[row][col].copy();
+            for(int col = count; col < MATERIAL_SIZE - count; col++) {
+                copy[(MATERIAL_SIZE - 1) - count][col] = new Material(material[(MATERIAL_SIZE - 1) - col][(MATERIAL_SIZE - 1) - count].quality, material[(MATERIAL_SIZE - 1) - col][(MATERIAL_SIZE - 1) - count].color);
+                copy[count][col] = new Material(material[(MATERIAL_SIZE - 1) - col][0].quality, material[(MATERIAL_SIZE - 1) - col][0].color);
+            }
         }
 
         return copy;
     }
 
-    static Material[][] rotate(Material[][] material) {
-        Material[][] newMaterial = new Material[material.length][material[0].length];
+    static class MaterialInfo {
+        int x;
+        int y;
+        int materialIdx;
+        int rotateIdx;
 
-        for(int row = 0; row < material.length; row++) {
-            for(int col = 0; col < material[row].length; col++)
-                newMaterial[col][(material.length - 1) - row] = material[row][col];
+        public MaterialInfo(int x, int y, int materialIdx, int rotateIdx) {
+            this.x = x;
+            this.y = y;
+            this.materialIdx = materialIdx;
+            this.rotateIdx = rotateIdx;
         }
-
-        return newMaterial;
     }
 
     static class Material {
-        int effect;
-        char element;
+        int quality = 0;
+        char color = 'W';
 
-        public Material(int effect) {
-            this.effect = effect;
+        public Material() {
+            quality = 0;
+            color = 'W';
         }
 
-        public Material(int effect, char element) {
-            this.effect = effect;
-            this.element = element;
+        public Material(int quality) {
+            this.quality = quality;
         }
 
-        public Material copy() {
-            return new Material(this.effect, this.element);
-        }
-
-        @Override
-        public String toString() {
-            return "Material{" +
-                    "effect=" + effect +
-                    ", element=" + element +
-                    '}';
+        public Material(int quality, char color) {
+            this.quality = quality;
+            this.color = color;
         }
     }
 
@@ -191,7 +266,7 @@ public class baekjun15898 {
             while(st == null || !st.hasMoreElements()) {
                 try {
                     st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
+                } catch(IOException e) {
                     e.printStackTrace();
                 }
             }
